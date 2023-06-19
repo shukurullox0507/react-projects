cf957e42de6db16c8d570416d74fa63a4307c755
shukurullox0507 <shukurullox0507@gmail.com>
2023-06-19 23:54:28 +0500

    last cmmit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Game from './components/game/game'
+
+function App() {
+  return (
+    <div className="container">
+      <Game/>
+    </div>
+  )
+}
+
+export default App
